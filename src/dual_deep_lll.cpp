a2a53b08bc5979b16e38a3068b2de2890bf752a9
yaito3014 <ykakeyama3014@gmail.com>
2025-06-20 05:32:38 +0900

    add required headers

--- src/dual_deep_lll.cpp
+++ src/dual_deep_lll.cpp
@@ -2,6 +2,14 @@
 
 #include "core.h"
 
+#include <math.h>
+
+#include <algorithm>
+#include <exception>
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+
 template <class T>
 void Lattice<T>::dualDeepLLL(const double delta, const bool compute_gso)
 {
