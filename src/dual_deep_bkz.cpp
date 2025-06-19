ab22aa415dc8c590d791a39b1886a5c732ae6790
yaito3014 <ykakeyama3014@gmail.com>
2025-06-20 05:31:15 +0900

    add required headers

--- src/dual_deep_bkz.cpp
+++ src/dual_deep_bkz.cpp
@@ -2,6 +2,14 @@
 
 #include "core.h"
 
+#include <stdio.h>
+
+#include <algorithm>
+#include <exception>
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+
 template<class T>
 void Lattice<T>::dualDeepBKZ(const long beta, const double delta, const bool compute_gso)
 {
